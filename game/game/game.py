7151ede9702ac20d27f6262e8087fe4ff97046c7
Klaudiusz <klaudiusz.s1405@gmail.com>
2023-06-09 19:08:46 +0200

    09.06 work

--- game/game/game.py
+++ game/game/game.py
@@ -1,31 +1,65 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 import rclpy
 from rclpy.node import Node
-from turtlesim.srv import Spawn
+from turtlesim.srv import Spawn, TeleportAbsolute, SetPen
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
 
-from math import atan2, pow, sqrt
+from math import pow, sqrt
 from random import randint
 
 
 class Game(Node):
-
+    
     def __init__(self):
         super().__init__('game')
         
-        self.vel_x = 2.0
+        # VARIABLE INITIALIZATION
+        self.vel_x = -2.0
         self.vel_y = randint(-200, 200)/100
-        self.angle = atan2(self.vel_y, self.vel_x)
+        
+        self.ball_pose = Pose()
+        self.computer_pose = Pose()
+        self.player_pose = Pose()
 
+        # CONNECT WITH SPAWN SERVICES
         self.spawn_cli = self.create_client(Spawn, 'spawn')
 
         while not self.spawn_cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Service not available, waiting again...')
 
         self.spawn_turtle_request = Spawn.Request()
+        
+        self.send_spawn_turtle_computer_request()
+        self.send_spawn_turtle_player_request()
+        
+        # CONNECT WITH SET_PEN SERVICES
+        self.set_pen_ball_cli = self.create_client(SetPen, 'turtle1/set_pen')
 
+        while not self.set_pen_ball_cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+            
+        self.set_pen_computer_cli = self.create_client(SetPen, 'computer/set_pen')
+
+        while not self.set_pen_computer_cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+            
+        self.set_pen_player_cli = self.create_client(SetPen, 'player/set_pen')
+
+        while not self.set_pen_player_cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+
+        self.set_pen_request = SetPen.Request()
+        
+        # CONNECT WITH TELEPORT SERVICES
+        self.teleport_cli = self.create_client(TeleportAbsolute, 'turtle1/teleport_absolute')
+
+        while not self.teleport_cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+
+        self.teleport_turtle_request = TeleportAbsolute.Request()
+
+        # CREATE SUBSCRIBERS TO POSE TOPICS
         self.ball_pose_sub = self.create_subscription(Pose, 'turtle1/pose', self.ball_pose_sub_callback, 10)
         self.ball_pose_sub
 
@@ -35,43 +69,66 @@ class Game(Node):
         self.player_pose_sub = self.create_subscription(Pose, 'player/pose', self.player_pose_sub_callback, 10)
         self.player_pose_sub
 
+        # CREATE PUBLISHERS ON CMD_VEL TOPICS
         self.ball_pose_pub = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)        
         self.computer_pose_pub = self.create_publisher(Twist, 'computer/cmd_vel', 10)
         self.player_pose_pub = self.create_publisher(Twist, 'player/cmd_vel', 10)
         
-        self.send_spawn_turtle_computer_request()
-        self.send_spawn_turtle_player_request()
+        # SWITCH OFF PENS
+        self.send_set_pen_ball_request()
+        self.send_set_pen_computer_request()
+        self.send_set_pen_player_request()
         
+        # RUN CALLBACK FUNCTION (GAME LOOP)
         self.timer = self.create_timer(1 / 30, self.ball_pose_pub_callback)
 
+        # PROMPT USER
         self.get_logger().info('Game has been started.')
 
-    def ball_pose_pub_callback(self):
+
+    # GAME LOOP FUNCTION
+    def ball_pose_pub_callback(self):        
         distance_from_computer = sqrt(pow(self.ball_pose.x - self.computer_pose.x, 2) + pow(self.ball_pose.y - self.computer_pose.y, 2))
         distance_from_player = sqrt(pow(self.ball_pose.x - self.player_pose.x, 2) + pow(self.ball_pose.y - self.player_pose.y, 2))
         
+        # COMPUTER TURTLE AI
         if self.ball_pose.y > self.computer_pose.y:
             computer_msg = Twist()
-            computer_msg.linear.y = self.ball_pose.y / 2
+            computer_msg.linear.y = 2.0
             self.computer_pose_pub.publish(computer_msg)
         elif self.ball_pose.y < self.computer_pose.y:
             computer_msg = Twist()
-            computer_msg.linear.y = self.ball_pose.y / 2
+            computer_msg.linear.y = - 2.0
             self.computer_pose_pub.publish(computer_msg)
         
+        # BOUNCE FROM COMPUTER TURTLE
+        if distance_from_computer < 0.5 and self.computer_pose.x < self.ball_pose.x:
+            self.vel_x = abs(self.vel_x)
+        
+        # BOUNCE FROM PLAYER TURTLE
+        if distance_from_player < 0.5 and self.player_pose.x > self.ball_pose.x:
+            self.vel_x = - abs(self.vel_x)
+            
+        # BOUNCE FROM TOP/BOTTOM WALL
+        if self.ball_pose.y > 10.5:
+            self.vel_y = - abs(self.vel_y)
+        elif self.ball_pose.y < 1.0:
+            self.vel_y = abs(self.vel_y)
+        
+        # DETECT SCORING
+        if self.ball_pose.x > 10.5:
+            self.send_teleport_turtle_request()
+            self.get_logger().info('Computer scored.')
+        elif self.ball_pose.x < 1.0:
+            self.send_teleport_turtle_request()
+            self.get_logger().info('Player scored.')
+        
+        # MOVE BALL
         ball_msg = Twist()      
         ball_msg.linear.x = self.vel_x
         ball_msg.linear.y = self.vel_y      
         self.ball_pose_pub.publish(ball_msg)
 
-    def ball_pose_sub_callback(self, msg):
-        self.ball_pose = msg
-
-    def computer_pose_sub_callback(self, msg):
-        self.computer_pose = msg
-
-    def player_pose_sub_callback(self, msg):
-        self.player_pose = msg
 
     def send_spawn_turtle_computer_request(self):
         self.spawn_turtle_request.x = 1.0
@@ -79,10 +136,11 @@ class Game(Node):
         self.spawn_turtle_request.theta = 0.0
         self.spawn_turtle_request.name = 'computer'
 
-        self.future = self.spawn_cli.call_async(self.spawn_turtle_request)
-        rclpy.spin_until_future_complete(self, self.future)
+        self.future1 = self.spawn_cli.call_async(self.spawn_turtle_request)
+        rclpy.spin_until_future_complete(self, self.future1)
 
-        return self.future.result()
+        return self.future1.result()
+
 
     def send_spawn_turtle_player_request(self):
         self.spawn_turtle_request.x = 10.0
@@ -90,10 +148,59 @@ class Game(Node):
         self.spawn_turtle_request.theta = 3.14
         self.spawn_turtle_request.name = 'player'
 
-        self.future = self.spawn_cli.call_async(self.spawn_turtle_request)
-        rclpy.spin_until_future_complete(self, self.future)
+        self.future2 = self.spawn_cli.call_async(self.spawn_turtle_request)
+        rclpy.spin_until_future_complete(self, self.future2)
 
-        return self.future.result()
+        return self.future2.result()
+
+
+    def send_set_pen_ball_request(self):
+        self.set_pen_request.off = 1
+
+        self.future4 = self.set_pen_ball_cli.call_async(self.set_pen_request)
+        rclpy.spin_until_future_complete(self, self.future4)
+
+        return self.future4.result()
+
+
+    def send_set_pen_computer_request(self):
+        self.set_pen_request.off = 1
+
+        self.future5 = self.set_pen_computer_cli.call_async(self.set_pen_request)
+        rclpy.spin_until_future_complete(self, self.future5)
+
+        return self.future5.result()
+
+
+    def send_set_pen_player_request(self):
+        self.set_pen_request.off = 1
+
+        self.future6 = self.set_pen_player_cli.call_async(self.set_pen_request)
+        rclpy.spin_until_future_complete(self, self.future6)
+
+        return self.future6.result()
+
+
+    def send_teleport_turtle_request(self):
+        self.teleport_turtle_request.x = 5.5
+        self.teleport_turtle_request.y = 5.5
+
+        self.future3 = self.teleport_cli.call_async(self.teleport_turtle_request)
+        rclpy.spin_until_future_complete(self, self.future3)
+
+        return self.future3.result()
+
+
+    def ball_pose_sub_callback(self, msg):
+        self.ball_pose = msg
+
+
+    def computer_pose_sub_callback(self, msg):
+        self.computer_pose = msg
+
+
+    def player_pose_sub_callback(self, msg):
+        self.player_pose = msg
 
 
 def main(args=None):
